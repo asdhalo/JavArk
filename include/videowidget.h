173e8a457cbfb20285981160d13e13be4e169f22
asdhalo <asdhalo@qq.com>
2025-08-09 14:57:03 +0800

    Update project files

--- include/videowidget.h
+++
@@ -1,7 +0,0 @@
-void setUseFanartMode(bool useFanart);
-void setThumbnailSize(int size);
-std::shared_ptr<VideoItem> getVideoItem() const { return m_video; }
-void updateThumbnail();
-
-protected:
-void paintEvent(QPaintEvent *event) override; 
